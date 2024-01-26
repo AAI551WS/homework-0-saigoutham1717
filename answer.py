c302498ca8bc0c272bce8720683e9415bdb565b3
saigoutham1717 <157447747+saigoutham1717@users.noreply.github.com>
2024-01-26 12:04:18 -0500

    Update answer.py
    updated the code

--- answer.py
+++ answer.py
@@ -2,16 +2,15 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+    prints 'Hello, World!' to the console.
     """
 
     # Write a comment; comments are very useful to other programmers.
-
+    # prints hello world command
     # Write a Python statement to print Hello, World!
-
+    print("Hello, World!")
 def add(a,b):
-
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
